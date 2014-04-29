288f091b2392fed5d29e4589ff0d63db4908ba71
coni2k <serkanholat@hotmail.com>
2014-04-29 15:04:22 +0300

    list_template_issue

---
+++ BusinessObjects/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BusinessObjects
+{
+    public abstract class BaseEntity : IEntity
+    {
+        internal BaseEntity()
+        {
+            CreatedOn = DateTime.UtcNow;
+            ModifiedOn = DateTime.UtcNow;
+        }
+
+        public DateTime CreatedOn { get; set; }
+        public DateTime ModifiedOn { get; set; }
+        public Nullable<DateTime> DeletedOn { get; set; }
+    }
+}
---
+++ Facade.Tests/LicenseUnitOfWorkTest.cs
@@ -0,0 +1,67 @@
+﻿namespace Facade.Tests
+{
+    using BusinessObjects;
+    using Facade;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using System;
+    using System.Linq;
+
+    [TestClass]
+    public class LicenseUnitOfWorkTest
+    {
+        [TestMethod]
+        public void Select()
+        {
+            using (var unitOfWork = new LicenseUnitOfWork())
+            {
+                var licenses = unitOfWork.AllLive.AsEnumerable();
+            }
+        }
+
+        [TestMethod]
+        public void Insert()
+        {
+            ResourcePool sampleResourcePool;
+            using (var unitOfWork = new ResourcePoolUnitOfWork())
+                sampleResourcePool = unitOfWork.AllLive.OrderByDescending(item => item.CreatedOn).First();
+
+            using (var unitOfWork = new LicenseUnitOfWork())
+            {
+                var license = new License()
+                {
+                    ResourcePool = sampleResourcePool,
+                    Name = "New license",
+                    Text = "Text of new license"
+                };
+
+                unitOfWork.Insert(license);
+                unitOfWork.Save();
+            }
+        }
+
+        [TestMethod]
+        public void Update()
+        {
+            using (var unitOfWork = new LicenseUnitOfWork())
+            {
+                var license = unitOfWork.AllLive.OrderByDescending(item => item.CreatedOn).First();
+                license.Description += string.Format("{0}Update test: {1:yyyyMMdd_HHmmss}", Environment.NewLine, DateTime.Now);
+
+                unitOfWork.Update(license);
+                unitOfWork.Save();
+            }
+        }
+
+        [TestMethod]
+        public void Delete()
+        {
+            using (var unitOfWork = new LicenseUnitOfWork())
+            {
+                var license = unitOfWork.AllLive.OrderByDescending(item => item.CreatedOn).First();
+
+                unitOfWork.Delete(license.Id);
+                unitOfWork.Save();
+            }
+        }
+    }
+}
