57b164d4212aafa5ab8fc32b9d9d6ccdcb10546f
coni2k <serkanholat@hotmail.com>
2014-05-04 17:16:12 +0300

    0.10.1

---
+++ Web/Controllers/OData/BaseController.cs
@@ -0,0 +1,35 @@
+﻿using BusinessObjects;
+using Facade;
+using Microsoft.AspNet.Identity;
+using System.Linq;
+using System.Web.Http.OData;
+
+namespace Web.Controllers.OData
+{
+    public abstract class BaseController : ODataController
+    {
+        internal string AspNetUserId
+        {
+            get
+            {
+                if (User == null)
+                    return string.Empty;
+                return User.Identity.GetUserId();
+            }
+        }
+
+        internal bool IsAdmin
+        {
+            get { return User.IsInRole("Administrator"); }
+        }
+
+        internal User ApplicationUser
+        {
+            get
+            {
+                using (var userUnitOfWork = new UserUnitOfWork())
+                    return userUnitOfWork.AllLive.Single(user => user.AspNetUserId == AspNetUserId);
+            }
+        }
+    }
+}
