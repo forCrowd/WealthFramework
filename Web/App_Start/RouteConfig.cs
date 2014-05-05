e5883379e25e83a0c74e2c7f7d78b88e89e5b1ed
coni2k <serkanholat@hotmail.com>
2014-05-05 16:38:46 +0300

    0.10.5

--- Web/App_Start/RouteConfig.cs
+++ Web/App_Start/RouteConfig.cs
@@ -15,9 +15,8 @@ namespace Web
 
             routes.MapRoute(
                 name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
-                namespaces: new string[] { "Web.Controllers.Mvc" });
+                url: "{*url}",
+                defaults: new { controller = "Home", action = "Index" });
         }
     }
 }
--- Web/Scripts/app/route.js
+++ Web/Scripts/app/route.js
@@ -4,111 +4,63 @@
     angular.module('main')
         .config(routeConfig);
 
-    function routeConfig($routeProvider) {
+    function routeConfig($routeProvider, $locationProvider) {
 
         // Routes
         $routeProvider
-            .when('/', { controller: getController(true), templateUrl: getTemplate(true, '/') })
-            .when('/new', { controller: getController(false), templateUrl: getTemplate(false, '/new') })
-            .when('/edit/:Id', { controller: getController(false), templateUrl: getTemplate(false, '/edit/:Id') })
 
-            .when('/License/', { controller: 'licenseListController as vm', templateUrl: 'ViewsNg/list/licenseList.html' })
-            .when('/License/new', { controller: 'licenseEditController as vm', templateUrl: 'ViewsNg/edit/licenseEdit.html' })
-            .when('/License/edit/:Id', { controller: 'licenseEditController as vm', templateUrl: 'ViewsNg/edit/LicenseEdit.html' })
-            .when('/Organization/', { controller: 'organizationListController as vm', templateUrl: 'ViewsNg/list/organizationList.html' })
-            .when('/Organization/new', { controller: 'organizationEditController as vm', templateUrl: 'ViewsNg/edit/organizationEdit.html' })
-            .when('/Organization/edit/:Id', { controller: 'organizationEditController as vm', templateUrl: 'ViewsNg/edit/OrganizationEdit.html' })
-            .when('/ResourcePool/', { controller: 'resourcePoolListController as vm', templateUrl: 'ViewsNg/list/resourcePoolList.html' })
-            .when('/ResourcePool/new', { controller: 'resourcePoolEditController as vm', templateUrl: 'ViewsNg/edit/resourcePoolEdit.html' })
-            .when('/ResourcePool/edit/:Id', { controller: 'resourcePoolEditController as vm', templateUrl: 'ViewsNg/edit/ResourcePoolEdit.html' })
-            .when('/Sector/', { controller: 'sectorListController as vm', templateUrl: 'ViewsNg/list/sectorList.html' })
-            .when('/Sector/new', { controller: 'sectorEditController as vm', templateUrl: 'ViewsNg/edit/sectorEdit.html' })
-            .when('/Sector/edit/:Id', { controller: 'sectorEditController as vm', templateUrl: 'ViewsNg/edit/SectorEdit.html' })
-            .when('/User/', { controller: 'userListController as vm', templateUrl: 'ViewsNg/list/userList.html' })
-            .when('/User/new', { controller: 'userEditController as vm', templateUrl: 'ViewsNg/edit/userEdit.html' })
-            .when('/User/edit/:Id', { controller: 'userEditController as vm', templateUrl: 'ViewsNg/edit/UserEdit.html' })
-            .when('/UserLicenseRating/', { controller: 'userLicenseRatingListController as vm', templateUrl: 'ViewsNg/list/userLicenseRatingList.html' })
-            .when('/UserLicenseRating/new', { controller: 'userLicenseRatingEditController as vm', templateUrl: 'ViewsNg/edit/userLicenseRatingEdit.html' })
-            .when('/UserLicenseRating/edit/:Id', { controller: 'userLicenseRatingEditController as vm', templateUrl: 'ViewsNg/edit/UserLicenseRatingEdit.html' })
-            .when('/UserOrganization/', { controller: 'userOrganizationListController as vm', templateUrl: 'ViewsNg/list/userOrganizationList.html' })
-            .when('/UserOrganization/new', { controller: 'userOrganizationEditController as vm', templateUrl: 'ViewsNg/edit/userOrganizationEdit.html' })
-            .when('/UserOrganization/edit/:Id', { controller: 'userOrganizationEditController as vm', templateUrl: 'ViewsNg/edit/UserOrganizationEdit.html' })
-            .when('/UserResourcePool/', { controller: 'userResourcePoolListController as vm', templateUrl: 'ViewsNg/list/userResourcePoolList.html' })
-            .when('/UserResourcePool/new', { controller: 'userResourcePoolEditController as vm', templateUrl: 'ViewsNg/edit/userResourcePoolEdit.html' })
-            .when('/UserResourcePool/edit/:Id', { controller: 'userResourcePoolEditController as vm', templateUrl: 'ViewsNg/edit/UserResourcePoolEdit.html' })
-            .when('/UserSectorRating/', { controller: 'userSectorRatingListController as vm', templateUrl: 'ViewsNg/list/userSectorRatingList.html' })
-            .when('/UserSectorRating/new', { controller: 'userSectorRatingEditController as vm', templateUrl: 'ViewsNg/edit/userSectorRatingEdit.html' })
-            .when('/UserSectorRating/edit/:Id', { controller: 'userSectorRatingEditController as vm', templateUrl: 'ViewsNg/edit/UserSectorRatingEdit.html' })
+            .when('/', { templateUrl: 'ViewsNg/home/index.html' })
+            .when('/Home', { templateUrl: 'ViewsNg/home/index.html' })
+            .when('/Overview', { templateUrl: 'ViewsNg/Home/Overview.html' })
+            .when('/Technologies', { templateUrl: 'ViewsNg/Home/Technologies.html' })
 
-            .when('/TotalCostIndex/', { controller: 'userResourcePoolController as vm', templateUrl: 'ViewsNg/UserResourcePool/TotalCostIndex.html' })
-            .when('/KnowledgeIndex/', { controller: 'userResourcePoolController as vm', templateUrl: 'ViewsNg/UserResourcePool/KnowledgeIndex.html' })
-            .when('/QualityIndex/', { controller: 'userResourcePoolController as vm', templateUrl: 'ViewsNg/UserResourcePool/QualityIndex.html' })
-            .when('/EmployeeSatisfactionIndex/', { controller: 'userResourcePoolController as vm', templateUrl: 'ViewsNg/UserResourcePool/EmployeeSatisfactionIndex.html' })
-            .when('/CustomerSatisfactionIndex/', { controller: 'userResourcePoolController as vm', templateUrl: 'ViewsNg/UserResourcePool/CustomerSatisfactionIndex.html' })
-            .when('/SectorIndex/', { controller: 'userResourcePoolController as vm', templateUrl: 'ViewsNg/UserResourcePool/SectorIndex.html' })
-            .when('/DistanceIndex/', { controller: 'userResourcePoolController as vm', templateUrl: 'ViewsNg/UserResourcePool/DistanceIndex.html' })
-            .when('/AllInOne/', { controller: 'userResourcePoolController as vm', templateUrl: 'ViewsNg/UserResourcePool/AllInOne.html' })
+            .when('/User/Login', { controller: 'loginController as vm', templateUrl: '/ViewsNg/User/Login.html' })
+            .when('/User/Register', { controller: 'registerController as vm', templateUrl: '/ViewsNg/User/Register.html' })
+            .when('/User/ChangePassword', { controller: 'changePasswordController as vm', templateUrl: '/ViewsNg/User/ChangePassword.html' })
 
-            .when('/User/Login/', { controller: 'loginController as vm', templateUrl: 'ViewsNg/User/Login.html' })
-            .when('/User/Register/', { controller: 'registerController as vm', templateUrl: 'ViewsNg/User/Register.html' })
-            .when('/User/ChangePassword/', { controller: 'changePasswordController as vm', templateUrl: 'ViewsNg/User/ChangePassword.html' })
+            .when('/License', { controller: 'licenseListController as vm', templateUrl: '/ViewsNg/list/licenseList.html' })
+            .when('/License/new', { controller: 'licenseEditController as vm', templateUrl: '/ViewsNg/edit/licenseEdit.html' })
+            .when('/License/edit/:Id', { controller: 'licenseEditController as vm', templateUrl: '/ViewsNg/edit/LicenseEdit.html' })
+            .when('/Organization', { controller: 'organizationListController as vm', templateUrl: '/ViewsNg/list/organizationList.html' })
+            .when('/Organization/new', { controller: 'organizationEditController as vm', templateUrl: '/ViewsNg/edit/organizationEdit.html' })
+            .when('/Organization/edit/:Id', { controller: 'organizationEditController as vm', templateUrl: '/ViewsNg/edit/OrganizationEdit.html' })
+            .when('/ResourcePool', { controller: 'resourcePoolListController as vm', templateUrl: '/ViewsNg/list/resourcePoolList.html' })
+            .when('/ResourcePool/new', { controller: 'resourcePoolEditController as vm', templateUrl: '/ViewsNg/edit/resourcePoolEdit.html' })
+            .when('/ResourcePool/edit/:Id', { controller: 'resourcePoolEditController as vm', templateUrl: '/ViewsNg/edit/ResourcePoolEdit.html' })
+            .when('/Sector', { controller: 'sectorListController as vm', templateUrl: '/ViewsNg/list/sectorList.html' })
+            .when('/Sector/new', { controller: 'sectorEditController as vm', templateUrl: '/ViewsNg/edit/sectorEdit.html' })
+            .when('/Sector/edit/:Id', { controller: 'sectorEditController as vm', templateUrl: '/ViewsNg/edit/SectorEdit.html' })
+            .when('/User', { controller: 'userListController as vm', templateUrl: '/ViewsNg/list/userList.html' })
+            .when('/User/new', { controller: 'userEditController as vm', templateUrl: '/ViewsNg/edit/userEdit.html' })
+            .when('/User/edit/:Id', { controller: 'userEditController as vm', templateUrl: '/ViewsNg/edit/UserEdit.html' })
+            .when('/UserLicenseRating', { controller: 'userLicenseRatingListController as vm', templateUrl: '/ViewsNg/list/userLicenseRatingList.html' })
+            .when('/UserLicenseRating/new', { controller: 'userLicenseRatingEditController as vm', templateUrl: '/ViewsNg/edit/userLicenseRatingEdit.html' })
+            .when('/UserLicenseRating/edit/:Id', { controller: 'userLicenseRatingEditController as vm', templateUrl: '/ViewsNg/edit/UserLicenseRatingEdit.html' })
+            .when('/UserOrganization', { controller: 'userOrganizationListController as vm', templateUrl: '/ViewsNg/list/userOrganizationList.html' })
+            .when('/UserOrganization/new', { controller: 'userOrganizationEditController as vm', templateUrl: '/ViewsNg/edit/userOrganizationEdit.html' })
+            .when('/UserOrganization/edit/:Id', { controller: 'userOrganizationEditController as vm', templateUrl: '/ViewsNg/edit/UserOrganizationEdit.html' })
+            .when('/UserResourcePool', { controller: 'userResourcePoolListController as vm', templateUrl: '/ViewsNg/list/userResourcePoolList.html' })
+            .when('/UserResourcePool/new', { controller: 'userResourcePoolEditController as vm', templateUrl: '/ViewsNg/edit/userResourcePoolEdit.html' })
+            .when('/UserResourcePool/edit/:Id', { controller: 'userResourcePoolEditController as vm', templateUrl: '/ViewsNg/edit/UserResourcePoolEdit.html' })
+            .when('/UserSectorRating', { controller: 'userSectorRatingListController as vm', templateUrl: '/ViewsNg/list/userSectorRatingList.html' })
+            .when('/UserSectorRating/new', { controller: 'userSectorRatingEditController as vm', templateUrl: '/ViewsNg/edit/userSectorRatingEdit.html' })
+            .when('/UserSectorRating/edit/:Id', { controller: 'userSectorRatingEditController as vm', templateUrl: '/ViewsNg/edit/UserSectorRatingEdit.html' })
 
-            .when('/Overview', {
-                templateUrl: 'ViewsNg/Home/Overview.html'
-            })
-            .when('/Technologies', {
-                templateUrl: 'ViewsNg/Home/Technologies.html'
-            }).otherwise({
-                redirectTo: '/'
-            });
+            .when('/TotalCostIndex', { controller: 'userResourcePoolController as vm', templateUrl: '/ViewsNg/UserResourcePool/TotalCostIndex.html' })
+            .when('/KnowledgeIndex', { controller: 'userResourcePoolController as vm', templateUrl: '/ViewsNg/UserResourcePool/KnowledgeIndex.html' })
+            .when('/QualityIndex', { controller: 'userResourcePoolController as vm', templateUrl: '/ViewsNg/UserResourcePool/QualityIndex.html' })
+            .when('/EmployeeSatisfactionIndex', { controller: 'userResourcePoolController as vm', templateUrl: '/ViewsNg/UserResourcePool/EmployeeSatisfactionIndex.html' })
+            .when('/CustomerSatisfactionIndex', { controller: 'userResourcePoolController as vm', templateUrl: '/ViewsNg/UserResourcePool/CustomerSatisfactionIndex.html' })
+            .when('/SectorIndex', { controller: 'userResourcePoolController as vm', templateUrl: '/ViewsNg/UserResourcePool/SectorIndex.html' })
+            .when('/DistanceIndex', { controller: 'userResourcePoolController as vm', templateUrl: '/ViewsNg/UserResourcePool/DistanceIndex.html' })
+            .when('/AllInOne', { controller: 'userResourcePoolController as vm', templateUrl: '/ViewsNg/UserResourcePool/AllInOne.html' })
 
-        function getEntityName() {
+            .otherwise({ redirectTo: '/' });
 
-            var entityName = window.location.pathname.replace('/', '').replace('Ng', '');
-
-            var entityNameCamelCase = entityName.length > 1
-                ? entityName[0].toLowerCase() + entityName.substring(1)
-                : entityName;
-
-            return entityNameCamelCase;
-        }
-
-        function getController(isList) {
-
-            var isEntityRoute = window.location.pathname.indexOf('Ng') > 0;
-
-            if (isEntityRoute)
-                return isList
-                    ? getEntityName() + 'ListController as vm'
-                    : getEntityName() + 'EditController as vm';
-            else
-                return null;
-        }
-
-        function getTemplate(isList, routeState) {
-
-            var isEntityRoute = window.location.pathname.indexOf('Ng') > 0;
-
-            if (isEntityRoute)
-                return isList
-                ? 'ViewsNg/list/' + getEntityName() + 'List.html'
-                : 'ViewsNg/edit/' + getEntityName() + 'Edit.html';
-            else {
-                var folder = 'home';
-                var template = 'index.html';
-
-                var pathParams = window.location.pathname.substring(1).split('/');
-
-                if (pathParams.length == 2) {
-                    folder = pathParams[0];
-                    template = pathParams[1] + '.html';
-                }
-
-                var template = 'ViewsNg/' + folder + '/' + template;
-
-                return template;
-            }
+        // Html5Mode is on, if supported (# will not be used)
+        if (window.history && window.history.pushState) {
+            console.log('$locationProvider.html5Mode(true)');
+            $locationProvider.html5Mode(true);
         }
     }
-
 })();
