3b6b222e13a28c60b634282eb3f8258ef0b24bf7
coni2k <serkanholat@hotmail.com>
2014-05-08 15:02:44 +0300

    0.10.8

--- SolutionItems/Scripts/Build/IISExpress.bat
+++ SolutionItems/Scripts/Build/IISExpress.bat
@@ -7,7 +7,11 @@ REM Confirmation
 CHOICE /M "Are you sure you want to run the script"
 IF ERRORLEVEL 2 EXIT
 
+REM Change the current directory
+cd /d %~dp0\..\..\..
+
 echo.
-"C:\Program Files\IIS Express\iisexpress" /path:"%~dp0\..\..\..\Web" /port:10001 /systray:true
+REM TODO IIS Express folder could differ
+"C:\Program Files\IIS Express\iisexpress" /path:%cd%\Web /port:10001 /systray:true
 echo.
 pause
--- SolutionItems/Scripts/Build/MSBuild.bat
+++ SolutionItems/Scripts/Build/MSBuild.bat
@@ -8,6 +8,7 @@ CHOICE /M "Are you sure you want to run the script"
 IF ERRORLEVEL 2 EXIT
 
 echo.
+REM TODO MSBuild folder could differ
 C:\Windows\Microsoft.NET\Framework64\v4.0.30319\msbuild "%~dp0\..\..\..\Wealtheconomy.sln" /v:q
 
 echo Script execution completed
