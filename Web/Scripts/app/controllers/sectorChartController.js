1519f3c9f3bd3505a9fe31b263975445b3f8e44d
coni2k <serkanholat@hotmail.com>
2014-05-12 17:31:41 +0300

    0.11

--- Web/Scripts/app/controllers/sectorChartController.js
+++ Web/Scripts/app/controllers/sectorChartController.js
@@ -8,57 +8,94 @@
     function sectorChartController(userSectorRatingService, logger) {
         logger = logger.forSource(controllerId);
 
-        var chartData = [];
-
         var vm = this;
-        vm.increase = increase;
+        vm.chartData = null;
         vm.decrease = decrease;
+        vm.increase = increase;
+        vm.resetChanges = resetChanges;
+        vm.saveChanges = saveChanges;
 
         initialize();
+
+        /* Implementations */
 
         function initialize() {
             configureChart();
             loadChartData();
         }
 
-        function increase() {
-            chartData[0].y += 10;
+        function decrease(index) {
+            vm.chartData[index].y -= 5;
         }
 
-        function decrease() {
-            chartData[0].y -= 10;
+        function increase(index) {
+            vm.chartData[index].y += 5;
         }
 
         function configureChart() {
             vm.chartConfig = {
                 title: {
-                    text: 'Sectors'
+                    //text: 'Sectors'
+                    text: ''
                 },
                 options: {
                     chart: {
                         type: 'pie'
                     }
-                },
-                loading: true
+                }
             }
         }
 
         function loadChartData() {
 
+            vm.chartConfig.loading = true;
+
             userSectorRatingService.getUserSectorRatingSet(false)
                 .then(function (data) {
 
-                    // Convert the data and set it
+                    // Convert userSectorRating to chart data
+                    vm.chartData = [];
                     for (var i = 0; i < data.length; i++) {
                         var chartDataItem = {
                             name: data[i].Sector.Name,
                             y: data[i].Rating
                         }
-                        chartData.push(chartDataItem);
+                        vm.chartData.push(chartDataItem);
                     }
 
-                    vm.chartConfig.series = [{ data: chartData }];
+                    vm.chartConfig.series = [{ data: vm.chartData }];
                     vm.chartConfig.loading = false;
+                });
+        }
+
+        function resetChanges() {
+            loadChartData();
+        }
+
+        function saveChanges() {
+
+            userSectorRatingService.getUserSectorRatingSet(false)
+                .then(function (data) {
+
+                    // Convert chart data to userSectorRating
+                    for (var i = 0; i < vm.chartData.length; i++) {
+                        var dataItem = data[i];
+                        var chartDataItem = vm.chartData[i];
+
+                        if (dataItem.Rating !== chartDataItem.y)
+                        {
+                            dataItem.Rating = chartDataItem.y;
+
+                            var rowVersion = dataItem.RowVersion;
+                            dataItem.RowVersion = '';
+                            dataItem.RowVersion = rowVersion;
+                        }
+                    }
+
+                    userSectorRatingService.saveChanges()
+                        .then(function () {
+                            logger.logSuccess('Your changes have been saved!', null, true);
+                        });
                 });
         }
     };
