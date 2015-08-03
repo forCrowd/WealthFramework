1a18caac15271f604544dbfd21e78a644b7b15ac
coni2k <serkanholat@hotmail.com>
2015-08-03 13:31:28 +0300

    0.35

--- BusinessObjects/Models/ResourcePool.cs
+++ BusinessObjects/Models/ResourcePool.cs
@@ -59,11 +59,15 @@ namespace forCrowd.WealthEconomy.BusinessObjects
         [DisplayOnEditView(true)]
         public bool UseFixedResourcePoolRate { get; set; }
 
+        // TODO Doesn't have to be nullable but it requires a default value then which needs to be done
+        // by manually editing migration file which is not necessary at the moment / SH - 03 Aug. '15
         [DisplayOnListView(false)]
         [DisplayOnEditView(false)]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
         public decimal? ResourcePoolRateTotal { get; private set; }
 
+        // TODO Doesn't have to be nullable but it requires a default value then which needs to be done
+        // by manually editing migration file which is not necessary at the moment / SH - 03 Aug. '15
         [DisplayOnListView(false)]
         [DisplayOnEditView(false)]
         [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
