59e579fd318b09f479cc763fe7aadfa439d95226
coni2k <serkanholat@hotmail.com>
2016-01-07 12:57:25 +0200

    0.41.4

--- SolutionItems/Properties/AssemblyInfo.cs
+++ SolutionItems/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 //
 // AssemblyFileVersion is not in use for the moment
 //
-[assembly: AssemblyVersion("0.41.3")]
+[assembly: AssemblyVersion("0.41.4")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
