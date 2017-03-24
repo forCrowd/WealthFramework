7517298fb6d5685a8024705632975116a82fe466
coni2k <serkanholat@hotmail.com>
2017-03-25 00:02:17 +0100

    0.73.3 - coni2k

--- ng2Client/app/settings/settings.ts
+++ ng2Client/app/settings/settings.ts
@@ -27,5 +27,5 @@ export class Settings {
     /**
      * Application version number
      */
-    static get version(): string { return "0.73.2"; }
+    static get version(): string { return "0.73.3"; }
 }
