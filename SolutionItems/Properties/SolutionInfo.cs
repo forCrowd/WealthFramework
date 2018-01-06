b6f6bfb914be49c74d119f7d41b847f629a7a749
coni2k <coni2k@users.noreply.github.com>
2018-01-06 22:28:13 +0100

    0.82.4 - coni2k

--- SolutionItems/Properties/SolutionInfo.cs
+++ SolutionItems/Properties/SolutionInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Aiming to stick to Semver versioning: http://semver.org/spec/v2.0.0.html
-[assembly: AssemblyInformationalVersion("0.82.2")]
+[assembly: AssemblyInformationalVersion("0.82.4")]
